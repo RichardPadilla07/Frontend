4ae5e5ff95af012dc175b3e34cd3013b84658b2f
RichardPadilla07 <richard.padilla@epn.edu.ec>
2025-05-20 16:23:49 -0500

    COMMIT 4

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -1,28 +1,32 @@
 import axios from 'axios' // Axios es una librería para hacer peticiones HTTP
 import { toast } from 'react-toastify' // Librería para mostrar notificaciones
 
+
+// Base de mi custom hook
 function useFetch() {
 
+    // Para hacer peticiones a la backend y este debe darme una respuesta
     const fetchDataBackend = async (url, form = null, method = 'POST') => {
 
-        try {
-            let respuesta
-            if (method === 'POST') {
-                respuesta = await axios.post(url, form)
-            } else if (method === 'GET') {
-                respuesta = await axios.get(url)
+        try { // Intento hacer la petición
+            let respuesta // Variable para guardar la respuesta
+            if (method === 'POST') { // Si el método es POST, hago una petición POST
+                respuesta = await axios.post(url, form) // Enviando el form
+            } else if (method === 'GET') { // Si el método es GET, hago una petición GET
+                respuesta = await axios.get(url) // Enviando el form
             }
-            toast.success(respuesta?.data?.msg)
-            return respuesta?.data
+            toast.success(respuesta?.data?.msg) // Si la respuesta es correcta, muestro una notificación de éxito
+            return respuesta?.data // Devuelvo la respuesta
             
-        } catch (error) {
-            toast.error(error.response?.data?.msg)
-            const errorMsg = error.response?.data?.msg || 'An error occurred';
-            throw new Error(errorMsg);
+        } catch (error) { // Si hay un error, lo manejo aquí
+            toast.error(error.response?.data?.msg) // Si hay un error, lo muestro en la notificación
+            const errorMsg = error.response?.data?.msg || 'An error occurred'; // Mensaje de error por defecto
+            throw new Error(errorMsg); // Lanza un error para que el componente que llama a este hook lo maneje
         }
     }
 
-    return { fetchDataBackend }
+    return { fetchDataBackend } // Devuelvo la función fetchDataBackend para que la pueda usar en otros componentes
 }
 
+// Exporto el custom hook
 export default useFetch
